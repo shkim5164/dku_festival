0c9d293203dc1e5aa9caf1435505cb59cb7a8301
shkim5164 <gk457@naver.com>
2018-04-07 10:38:20 +0000

    0407 sunu2

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,10 +2,14 @@ class AdminController < ApplicationController
   def index
     # @products = Report.order('created_at DESC')
     # User.joins('LEFT OUTER JOIN reports ON reports.user_id = users.id')
-
-    # respond_to do |format|
-    #   format.html
-    #   format.xlsx
-    # end
+   
+    #이벤트 별로 보여주기
+    @event1 = Apply.where(event_id: 1)
+    @event2 = Apply.where(event_id: 2)
+    @event3 = Apply.where(event_id: 3)
+    respond_to do |format|
+      format.html
+      format.xlsx
+    end
   end
 end
