34a42d5d3febda8d340d70d73fceda810608f170
Seah <blueseaangel@likelion.org>
2018-04-29 05:20:09 +0000

    pull받기

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180325064143) do
+ActiveRecord::Schema.define(version: 20180330090224) do
 
   create_table "applies", force: :cascade do |t|
     t.integer  "how_many"
     t.string   "time"
     t.integer  "user_id"
     t.integer  "event_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "phone_number"
+    t.string   "student_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "events", force: :cascade do |t|
@@ -36,6 +38,23 @@ ActiveRecord::Schema.define(version: 20180325064143) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "simple_hashtag_hashtaggings", force: :cascade do |t|
+    t.integer "hashtag_id"
+    t.integer "hashtaggable_id"
+    t.string  "hashtaggable_type"
+  end
+
+  add_index "simple_hashtag_hashtaggings", ["hashtag_id"], name: "index_simple_hashtag_hashtaggings_on_hashtag_id"
+  add_index "simple_hashtag_hashtaggings", ["hashtaggable_id", "hashtaggable_type"], name: "index_hashtaggings_hashtaggable_id_hashtaggable_type"
+
+  create_table "simple_hashtag_hashtags", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "simple_hashtag_hashtags", ["name"], name: "index_simple_hashtag_hashtags_on_name"
+
   create_table "singers", force: :cascade do |t|
     t.string   "name"
     t.text     "info"
@@ -44,8 +63,6 @@ ActiveRecord::Schema.define(version: 20180325064143) do
     t.string   "place"
     t.string   "crass"
     t.integer  "day"
-    t.string   "w_day"
-    t.string   "w_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
