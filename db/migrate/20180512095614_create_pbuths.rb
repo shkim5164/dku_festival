d77a4a8c352792e70b67916c434f0c406241fc71
shkim5164 <gk457@naver.com>
2018-05-12 12:26:14 +0000

    0512 sunu last

---
+++ db/migrate/20180512095614_create_pbuths.rb
@@ -0,0 +1,12 @@
+class CreatePbuths < ActiveRecord::Migration
+  def change
+    create_table :pbuths do |t|
+       t.string :name # 주점이름
+      t.string :major # 주점학과
+      t.string :numbs # 번호부여
+      t.string :idd # 클래스 부여
+      t.string :madi # 주점 한 마디
+      t.timestamps null: false
+    end
+  end
+end
